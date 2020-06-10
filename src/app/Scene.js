4f620ad237248a772f17e81f19146951045e5473
José Victor Catalani <josevictorcatalani@gmail.com>
2020-06-10 20:03:35 -0300

    project items

--- src/app/Scene.js
+++ src/app/Scene.js
@@ -9,10 +9,8 @@ export default class Scene {
   setState() {
     this.scrollDocLocked = true;
     this.enteredSite = false;
-    this.homePanel = document.getElementById("home");
-    this.bioPanel = document.getElementById("bio");
-    this.panels = [this.homePanel, this.bioPanel];
     this.activePanelIndex = 0;
+    this.colors = ["none", "purple", "cyan"];
   }
 
   releaseScroll() {
@@ -29,7 +27,13 @@ export default class Scene {
   }
 
   setActivePanel(index) {
-    return this.panels[index];
+    this.activePanelIndex = index;
+  }
+
+  goToPanel(panelId) {
+    const index = this.panels.findIndex((panel) => panelId === panel.id);
+    this.setActivePanel(index);
+    this.animateChangingPanel();
   }
 
   goToNextPanel() {
@@ -51,30 +55,55 @@ export default class Scene {
     this.animateChangingPanel();
   }
 
+  getBackgroundColorClass() {
+    return this.colors[this.activePanelIndex];
+  }
+
   animateChangingPanel() {
     const panel = this.getActivePanel();
     const oldPanel = this.getOldPanel();
+    this.enteredSite = true;
+    this.releaseScroll();
 
-    oldPanel.classList.replace("active", "old");
-
+    oldPanel.classList.add("old");
+    this.main.classList.remove(...this.colors);
+    this.main.classList.add(this.getBackgroundColorClass());
+    this.window.scrollTo(0, 0);
     setTimeout(() => panel.classList.add("active"), 500);
+    setTimeout(() => oldPanel.classList.remove("old", "active"), 1000);
   }
 
   bindElements() {
     this.window = window;
     this.main = document.getElementById("main");
-    this.menusItems = document.querySelectorAll(".menu-items");
+    this.virtuesItems = document.querySelectorAll(".virtues-items");
+    this.menu = document.getElementById("menu");
+    this.menuLink = document.querySelectorAll("#menu a");
+    this.homePanel = document.getElementById("home");
+    this.projectsPanel = document.getElementById("projects");
+    this.bioPanel = document.getElementById("bio");
+    this.panels = [this.homePanel, this.bioPanel, this.projectsPanel];
   }
 
   setInitialAnimations() {
     setTimeout(
-      () => [...this.menusItems].map((menu) => menu.classList.add("start")),
+      () =>
+        [...this.virtuesItems].map((virtue) => virtue.classList.add("start")),
       1500
     );
   }
 
   setEvents() {
     this.window.addEventListener("wheel", (e) => this.onScroll(e));
+
+    [...this.menuLink].forEach((menuLink) =>
+      menuLink.addEventListener("click", (e) => {
+        e.preventDefault();
+        const panelToGo = menuLink.getAttribute("href").split("/").pop();
+        this.goToPanel(panelToGo);
+        return false;
+      })
+    );
   }
 
   onScroll(e) {
@@ -82,13 +111,12 @@ export default class Scene {
     if (!this.enteredSite) {
       this.enteredSite = true;
 
-      [...this.menusItems].map((menu) =>
-        menu.classList.replace("start", "leave")
+      [...this.virtuesItems].map((virtue) =>
+        virtue.classList.replace("start", "leave")
       );
 
       setTimeout(() => {
         this.goToNextPanel();
-        this.main.classList.add("purple");
         this.releaseScroll();
       }, 1000);
     }
